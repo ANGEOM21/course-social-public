5a1c48bd2f9269f96474e1aa2111aba37f566fee
ANGEOM21 <idrisanjounghan313@gmail.com>
2025-10-04 22:59:24 +0700

    update menu item

--- resources/views/components/sidebar/menu-item.blade.php
+++ resources/views/components/sidebar/menu-item.blade.php
@@ -1,23 +1,18 @@
 @props(['item'])
 
 @php
-  $currentRouteName = Route::currentRouteName();
-  $menuRouteName = $item['route'];
-  
-  $menuBase = Illuminate\Support\Str::remove('.index', $menuRouteName);
-  $currentBase = Illuminate\Support\Str::beforeLast($currentRouteName, '.');
+  use Illuminate\Support\Str;
 
-  $isActive = $menuBase === $currentBase;
+  $menuRouteName = $item['route'];
+  $menuBase = Str::replaceLast('.index', '', $menuRouteName);
+  $isActive = request()->routeIs($menuBase) || request()->routeIs($menuBase . '.*');
   $activeClass = 'bg-base-300/20 text-base-100 font-semibold active-menu rounded-lg';
   $inactiveClass = 'text-base-100 font-normal';
   $hoverClass = 'hover:bg-base-100 hover:text-base-content';
-
-  $classes =
-      'flex items-center gap-1 p-2 sidebar-link ' . ($isActive ? $activeClass : $inactiveClass) . ' ' . $hoverClass;
+  $classes = 'flex items-center gap-1 p-2 sidebar-link ' . ($isActive ? $activeClass : $inactiveClass) . ' ' . $hoverClass;
 @endphp
 
-<a class="{{ $classes }}" wire:navigate href="{{ route($item['route']) }}"
-  @if ($isActive) data-active="1" @endif>
+<a class="{{ $classes }}" wire:navigate href="{{ route($item['route']) }}" @if ($isActive) data-active="1" @endif>
   <i class="{{ $item['icon'] }} w-6 text-center"></i>
   <span class="capitalize">{{ $item['text'] }}</span>
 </a>
